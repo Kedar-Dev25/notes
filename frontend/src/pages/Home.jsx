99176e4b32ae84235d86fd126f6acae628481b57
Kedar-Dev25 <kedar.code7@gmail.com>
2026-08-16 21:08:11 +0530

    Updated the Fragmentation to div for implementation of CSS

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 function Home() {
    return(
-        <>
+        <div>
             <button>C++</button>
             <button>Digital Electronics</button>
             <button>Python</button>
             <button>Data Structure</button>
             <button>Algorithm</button>
-        </>
+        </div>
     );
 }
 export default Home;
